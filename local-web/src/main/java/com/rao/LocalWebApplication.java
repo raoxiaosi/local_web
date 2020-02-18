39c4f53d31af45d590c3f3b99531f5529559bd7d
raojing <1770561005@qq.com>
2020-02-18 15:25:24 +0800

    整合通用mapper和mybatis分页插件

--- local-web/src/main/java/com/rao/LocalWebApplication.java
+++ local-web/src/main/java/com/rao/LocalWebApplication.java
@@ -1,8 +1,8 @@
 package com.rao;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan(value = "com.rao.dao")
 @SpringBootApplication
