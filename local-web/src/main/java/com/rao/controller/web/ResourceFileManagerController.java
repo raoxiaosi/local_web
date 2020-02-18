907ced982196a6e9dae2050051803cb90f23962e
raojing <1770561005@qq.com>
2020-02-18 17:31:56 +0800

    调整接口位置

--- local-web/src/main/java/com/rao/controller/web/ResourceFileManagerController.java
+++ local-web/src/main/java/com/rao/controller/web/ResourceFileManagerController.java
@@ -3,6 +3,7 @@ package com.rao.controller.web;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.rao.config.LocalOssConfig;
+import com.rao.util.file.DownLoadUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.util.LinkedMultiValueMap;
@@ -12,10 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import pojo.entity.resource.ResourceLocationsConfig;
+import pojo.entity.resource.ResourceVideo;
+import pojo.entity.resource.ServicePath;
 import pojo.vo.file.FileUploadVO;
+import service.resource.ResourceLocationsConfigService;
+import service.resource.ResourceVideoService;
+import service.resource.ServicePathService;
 import util.result.ResultMessage;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.util.UUID;
 
@@ -34,6 +42,12 @@ public class ResourceFileManagerController {
     private RestTemplate restTemplate;
     @Resource
     private LocalOssConfig localOssConfig;
+    @Resource
+    private ResourceVideoService resourceVideoService;
+    @Resource
+    private ServicePathService servicePathService;
+    @Resource
+    private ResourceLocationsConfigService resourceLocationsConfigService;
 
     /**
      * 文件上传
@@ -48,7 +62,7 @@ public class ResourceFileManagerController {
         String os = System.getProperty("os.name");
         String restApi = os.toLowerCase().startsWith("win") ? localOssConfig.getWdRestApi() : localOssConfig.getLmRestApi();
         File tempFile = new File("");
-        try{
+        try {
             // 在当前服务器中生成临时文件
             String originalFilename = UUID.randomUUID().toString() + file.getOriginalFilename();
             String classpath = ResourceUtils.getURL("classpath:").getPath();
@@ -67,11 +81,41 @@ public class ResourceFileManagerController {
             // 删除当前服务器的临时文件
             tempFile.delete();
             return ResultMessage.success().add("uploadInfo", fileUploadVO).addMessage("文件上传成功");
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResultMessage.fail().addMessage("文件上传失败");
-        }finally {
+        } finally {
             tempFile.delete();
         }
+    }
+
+    /**
+     * 资源下载
+     */
+    @RequestMapping("/download")
+    public String downloadFile(Long resourceId, HttpServletResponse response) throws Exception {
+        //获取资源信息
+        ResourceVideo video = resourceVideoService.find(resourceId);
+        if (video == null) {
+            return null;
+        }
+        //获取文件地址信息
+        ServicePath servicePath = servicePathService.find(video.getServiceId());
+        ResourceLocationsConfig config = resourceLocationsConfigService.find(servicePath.getConfigId());
+        String fileName = video.getVideoPath();
+
+        String os = System.getProperty("os.name");
+        log.info("当前的系统为:{}", os);
+        String basePath = os.toLowerCase().startsWith("win") ? config.getWdLocationPath() : config.getLmLocationPath();
+        log.info("资源文件所在的路径:{}", basePath);
+        String dataAddress = basePath + "/" + fileName;
+
+        //设置文件路径
+        File file = new File(dataAddress);
+        // 如果文件名存在，则进行下载
+        if (file.exists()) {
+            DownLoadUtil.downLoad(response, file, fileName);
+        }
+        return null;
     }
 
 }
